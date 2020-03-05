502ea2ce1a7e238245198db6fe372bf534f2e21d
Chif Gergő <chifgeri97@gmail.com>
2020-03-05 20:32:11 +0100

    CUstomize Icons in tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MealsPage from './components/meals/MealsPage';
@@ -12,7 +12,40 @@ const Tab = createBottomTabNavigator();
  const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator      
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            switch(route.name) {
+              case 'Meals':
+                iconName = 'food-croissant';
+                break;
+              case 'Tracker':
+                iconName = 'ballot-outline';
+                break;
+              case 'Statistics':
+                iconName = 'chart-line';
+                break;
+              case 'Settings':
+                iconName = 'settings';
+                break;
+              default:
+                iconName = 'warehouse'
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={35} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          style: {
+            height: 60
+          },
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="Meals" component={MealsPage} />
         <Tab.Screen name="Tracker" component={TrackerPage} />
         <Tab.Screen name="Statistics" component={StatisticsPage} />
